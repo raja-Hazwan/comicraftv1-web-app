f50c718a6ba43cd66a5b02757a7233463d875c31
Hazwan-jack <rajahazwan02@gmail.com>
2024-10-22 00:36:48 +0800

    2nd commit

--- src/pages/components/Navbar/Navbar.tsx
+++ src/pages/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Flex } from '@chakra-ui/react';
+import Image from 'next/image';
 
 const Navbar:React.FC = () => {
     
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex>Navbar damn</Flex>
+        <Flex bg="white" height="50px" padding="6px 12px">
+            <Flex align="center">
+          <Image src="/images/logo.png" alt="Logo" width={175} height={30} />
+                </Flex>
         </Flex>
     )
 
