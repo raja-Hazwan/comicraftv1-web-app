247f59c00bc6e0360a623ef63a4e134e399f326f
Hazwan-jack <rajahazwan02@gmail.com>
2025-01-04 21:42:29 +0800

    files updated

--- src/pages/components/Posts/PostItem.tsx
+++ src/pages/components/Posts/PostItem.tsx
@@ -63,32 +63,11 @@ const PostItem: React.FC<PostItemProps> = ({
       border="1px solid"
       bg="white"
       borderColor="gray.300"
-      borderRadius="4px"
+      borderRadius="10px"
       _hover={{ borderColor: 'gray.500' }}
       cursor="pointer"
       onClick={() => onSelectPost && onSelectPost(post)}
     >
-      {/* Vote Section */}
-      <Flex direction="column" align="center" bg="gray.100" p={2} width="40px" borderRadius="3px 0px 0px 3px">
-        <Icon
-          as={userVoteValue === 1 ? IoArrowUpCircleSharp : IoArrowUpCircleOutline}
-          color={userVoteValue === 1 ? 'orange.400' : 'gray.400'}
-          fontSize={22}
-          cursor="pointer"
-          onClick={(event) => onVote(event, post, 1, post.communityId)}
-        />
-        <Text fontSize="9pt" fontWeight={600}>
-          {post.voteStatus}
-        </Text>
-        <Icon
-          as={userVoteValue === -1 ? IoArrowDownCircleSharp : IoArrowDownCircleOutline}
-          color={userVoteValue === -1 ? 'blue.400' : 'gray.400'}
-          fontSize={22}
-          cursor="pointer"
-          onClick={(event) => onVote(event, post, -1, post.communityId)}
-        />
-      </Flex>
-
       {/* Post Content */}
       <Flex direction="column" width="100%">
         {error && (
@@ -140,38 +119,43 @@ const PostItem: React.FC<PostItemProps> = ({
           )}
         </Stack>
 
-        {/* Actions Section */}
-        <Flex ml={1} mb={0.5} color="gray.500">
-          <Flex
-            align="center"
-            p="8px 10px"
-            borderRadius={4}
-            _hover={{ bg: 'gray.200' }}
-            cursor="pointer"
-          >
+        {/* Actions and Vote Section */}
+        <Flex ml={1} mb={0.5} color="gray.500" align="center">
+          {/* Upvote/Downvote */}
+          <Flex align="center" p="8px 10px" borderRadius={4} _hover={{ bg: 'gray.200' }} cursor="pointer">
+            <Icon
+              as={userVoteValue === 1 ? IoArrowUpCircleSharp : IoArrowUpCircleOutline}
+              color={userVoteValue === 1 ? 'orange.400' : 'gray.400'}
+              fontSize={22}
+              onClick={(event) => onVote(event, post, 1, post.communityId)}
+            />
+            <Text mx={2} fontSize="9pt" fontWeight={600}>
+              {post.voteStatus}
+            </Text>
+            <Icon
+              as={userVoteValue === -1 ? IoArrowDownCircleSharp : IoArrowDownCircleOutline}
+              color={userVoteValue === -1 ? 'blue.400' : 'gray.400'}
+              fontSize={22}
+              onClick={(event) => onVote(event, post, -1, post.communityId)}
+            />
+          </Flex>
+
+          {/* Comments */}
+          <Flex align="center" p="8px 10px" borderRadius={4} _hover={{ bg: 'gray.200' }} cursor="pointer">
             <Icon as={BsChat} mr={2} />
             <Text fontSize="9pt">{post.numberOfComments}</Text>
           </Flex>
-          <Flex
-            align="center"
-            p="8px 10px"
-            borderRadius={4}
-            _hover={{ bg: 'gray.200' }}
-            cursor="pointer"
-          >
+          {/* Share */}
+          <Flex align="center" p="8px 10px" borderRadius={4} _hover={{ bg: 'gray.200' }} cursor="pointer">
             <Icon as={IoArrowRedoOutline} mr={2} />
             <Text fontSize="9pt">Share</Text>
           </Flex>
-          <Flex
-            align="center"
-            p="8px 10px"
-            borderRadius={4}
-            _hover={{ bg: 'gray.200' }}
-            cursor="pointer"
-          >
+          {/* Save */}
+          <Flex align="center" p="8px 10px" borderRadius={4} _hover={{ bg: 'gray.200' }} cursor="pointer">
             <Icon as={IoBookmarkOutline} mr={2} />
             <Text fontSize="9pt">Save</Text>
           </Flex>
+          {/* Delete (if creator) */}
           {userIsCreator && (
             <Flex
               align="center"
