a8591f3716c1b47f0b12148a98c187821dd739e3
Hazwan-jack <rajahazwan02@gmail.com>
2025-01-07 22:07:16 +0800

    directory updated

--- src/pages/components/Navbar/Directory/Communities.tsx
+++ src/pages/components/Navbar/Directory/Communities.tsx
@@ -6,7 +6,9 @@ import { communityState } from '@/atoms/communitiesAtom';
 import { useRecoilValue } from 'recoil';
 import MenuListItem from './MenuListItem';
 import { GiBookAura } from 'react-icons/gi';
-import { AiFillHome } from 'react-icons/ai'; // Importing Home icon
+import { AiFillHome } from 'react-icons/ai';
+import { FaFire } from 'react-icons/fa'; // Changed to FaFire
+import { IoInformationCircle } from 'react-icons/io5';
 
 const Communities: React.FC = () => {
   const [open, setOpen] = useState(false);
@@ -19,18 +21,30 @@ const Communities: React.FC = () => {
   return (
     <>
       <CreateCommunityModal open={open} handleClose={() => setOpen(false)} />
-
-      {/* Home Link */}
+      {/* Navigation Links */}
       <Box mt={3} mb={4}>
         <MenuListItem
           key="home"
-          icon={AiFillHome} // Home icon
+          icon={AiFillHome}
           displayText="Home"
           link="/"
           iconColor="black"
         />
+        <MenuListItem
+          key="trending"
+          icon={FaFire} // Changed from HiTrendingUp to FaFire
+          displayText="Trending"
+          link="/trending"
+          iconColor="black"
+        />
+        <MenuListItem
+          key="about"
+          icon={IoInformationCircle}
+          displayText="About"
+          link="/about"
+          iconColor="black"
+        />
       </Box>
-
       {/* Moderating Communities */}
       <Box mt={3} mb={4}>
         <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
@@ -49,7 +63,6 @@ const Communities: React.FC = () => {
             />
           ))}
       </Box>
-
       {/* My Communities */}
       <Box mt={3} mb={4}>
         <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
