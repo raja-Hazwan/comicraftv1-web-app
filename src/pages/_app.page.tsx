f65b8ab3e977ae094c39ab7d1e7288ae60b3f30b
Hazwan-jack <rajahazwan02@gmail.com>
2025-01-07 23:42:31 +0800

    title updated

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,18 +1,29 @@
-
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "@/pages/chakra/theme";
 import Layout from "./components/Layout/Layout";
 import { RecoilRoot } from "recoil";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-    <ChakraProvider theme={theme}>
-      <Layout>
-      <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <Head>
+          {/* Set the title */}
+          <title>ComiCraft : Arts Meet Community</title>
+          
+          {/* Set the favicon */}
+          <link rel="icon" href="/images/favicon.ico" />
+          
+          {/* Optional: Meta tags for SEO and responsiveness */}
+          <meta name="description" content="ComiCraft: A place where art meets community. Share and explore comics, art, and creativity!" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
     </RecoilRoot>
   );
 }
